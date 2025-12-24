1c5071fd1ac96628d2625df5403a2bced4c858d3
Axel <axelmychro@gmail.com>
2025-12-24 12:32:21 +0700

    delete .vercel

--- .vercel/output/functions/_ipx/[...].func/chunks/build/Overlay-styles.CHyWMQRB.mjs
+++
@@ -1,8 +0,0 @@
-const Overlay_vue_vue_type_style_index_0_scoped_e865f0e0_lang = ".overlays[data-v-e865f0e0]{transition:1.5s cubic-bezier(.6,0,.3,1)}";
-
-const OverlayStyles_CHyWMQRB = [
-  Overlay_vue_vue_type_style_index_0_scoped_e865f0e0_lang
-];
-
-export { OverlayStyles_CHyWMQRB as default };
-//# sourceMappingURL=Overlay-styles.CHyWMQRB.mjs.map
--- .vercel/output/functions/_ipx/[...].func/chunks/build/Projects-styles.bQvkbK1J.mjs
+++
@@ -1,8 +0,0 @@
-const Projects_vue_vue_type_style_index_0_scoped_590f3aa1_lang = "@media(min-width:40rem){@keyframes float-590f3aa1{0%,to{transform:translateY(3px) rotate(6deg)}50%{transform:translateY(-3px) rotate(3deg)}}.animate-float[data-v-590f3aa1]{animation:float-590f3aa1 3s ease-in-out infinite}}";
-
-const ProjectsStyles_bQvkbK1J = [
-  Projects_vue_vue_type_style_index_0_scoped_590f3aa1_lang
-];
-
-export { ProjectsStyles_bQvkbK1J as default };
-//# sourceMappingURL=Projects-styles.bQvkbK1J.mjs.map
